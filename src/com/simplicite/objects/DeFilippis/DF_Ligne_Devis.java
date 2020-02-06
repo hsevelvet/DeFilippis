4c64eb187674afc9896318601b6a732741dac69a
designer (Hamza SENOUSSI) <no-reply@simplicite.fr>
2020-02-06 14:23:40 +0000

    [5.0] HSE - Noms sur DF_Ligne_Devis.java

--- src/com/simplicite/objects/DeFilippis/DF_Ligne_Devis.java
+++ src/com/simplicite/objects/DeFilippis/DF_Ligne_Devis.java
@@ -20,58 +20,58 @@ public class DF_Ligne_Devis extends ObjectDB {
 		Integer id = Integer.valueOf(getRowId());
 		String id_ld = String.format("%04d",id);
 		
-		setFieldValue("df_ligne_devis_id",id_ld);
+		setFieldValue("defiLigneDevisId",id_ld);
 		
         // accès aux valeurs 
-        String unite = getField("df_produit_unite").getValue(); 
-        String des_produit = getField("df_produit_designation").getValue(); 
-        String fin_produit = getField("df_produit_finition").getValue();
+        String unite = getField("defiPrdUnite").getValue(); 
+        String des_produit = getField("defiPrdTypeProduit").getValue(); 
+        String fin_produit = getField("defiPrdFinitionFacesVues").getValue();
 
-		double mvp = getField("df_produit_masse_volumique").getDouble(0);
-		double lng = getField("df_produit_long").getDouble(0);
-		double lrg = getField("df_produit_larg").getDouble(0);
-		double ep = getField("df_produit_haut").getDouble(0);
-		int qte = getField("df_ligne_devis_quantite").getInt(0);
-		double dim_joint = getField("df_ligne_devis_dim_joints").getDouble(0);
-		double ptr = getField("df_ligne_devis_prix_transport_reference").getDouble(0);
-		double prc = getField("df_produit_prix").getDouble(0);
+		double mvp = getField("defiPrdMasseVolumique").getDouble(0);
+		double lng = getField("defiPrdLongueur").getDouble(0);
+		double lrg = getField("defiPrdLargeur").getDouble(0);
+		double ep = getField("defiPrdEpaisseur").getDouble(0);
+		int qte = getField("defiLigneDevisQuantite").getInt(0);
+		double dim_joint = getField("defiLigneDevisDimensionJoints").getDouble(0);
+		double ptr = getField("defiLigneDevisPrixTransportReference").getDouble(0);
+		double prc = getField("defiPrdPrixUnitaireHT").getDouble(0);
 		
 		// designation ligne devis
 		String designation = "Désignation Produit: "+ des_produit +"\t"+"Finition: "+ fin_produit+
 		"\n"+" Unité: " + unite +"\t" + " Longueur: "+lng +"\t" + " Largeur: "+lrg+"\t" +" Epaisseur: "+ep+"\t"  + "Dimension Joints: " +dim_joint;
 		
-		setFieldValue("df_ligne_devis_designation",designation);
+		setFieldValue("defiLigneDevisDesignation",designation);
 		
 		
 		// affectation du prix 
 		switch (unite){
 			case "T":
-				setFieldValue("df_ligne_devis_unite","m2");
-				setFieldValue("df_ligne_devis_prix_exw_t",prc);
+				setFieldValue("defiLigneDevisUnite","m2");
+				setFieldValue("defiLigneDevisPrixExwTonne",prc);
 				break;
 				
 			default :
-				setFieldValue("df_ligne_devis_prix_exw_u",prc);
+				setFieldValue("defiLigneDevisPrixExwUnitaire",prc);
 				break;
 		}
 		
-		Double pexwu = getField("df_ligne_devis_prix_exw_u").getDouble(0); 
-		Double pexwt = getField("df_ligne_devis_prix_exw_t").getDouble(0);
+		Double pexwu = getField("defiLigneDevisPrixExwUnitaire").getDouble(0); 
+		Double pexwt = getField("defiLigneDevisPrixExwTonne").getDouble(0);
 		
 		// calcul nombre d'éléments par unité sans joint
         if (lng == 0 || lrg == 0){
-        	setFieldValue("df_ligne_devis_nb_elt_ss_joint", 0);
+        	setFieldValue("defiLigneDevisNombreElementsSsJoints", 0);
         }
         else {
         	switch(unite){
 				case "m2":
-					setFieldValue("df_ligne_devis_nb_elt_ss_joint", 1/((lng / 100)*(lrg /100)));
+					setFieldValue("defiLigneDevisNombreElementsSsJoints", 1/((lng / 100)*(lrg /100)));
 					break;
 				case "ml":
-					setFieldValue("df_ligne_devis_nb_elt_ss_joint", 1/(lng / 100));
+					setFieldValue("defiLigneDevisNombreElementsSsJoints", 1/(lng / 100));
 					break;
 				case "u":
-					setFieldValue("df_ligne_devis_nb_elt_ss_joint", 1);
+					setFieldValue("defiLigneDevisNombreElementsSsJoints", 1);
 					break;
 			}
         }
@@ -79,98 +79,98 @@ public class DF_Ligne_Devis extends ObjectDB {
         // calcul masse unitaire sans joint
         switch(unite){
 			case "u":
-				setFieldValue("df_ligne_devis_masse_unitaire_ss_joint", mvp*(lng*lrg*ep / 1000000));
+				setFieldValue("defiLigneDevisMasseUnitaireSsJoints", mvp*(lng*lrg*ep / 1000000));
 				break;
 			case "ml":
-				setFieldValue("df_ligne_devis_masse_unitaire_ss_joint", mvp*(lng*ep / 10000));
+				setFieldValue("defiLigneDevisMasseUnitaireSsJoints", mvp*(lng*ep / 10000));
 				break;
 			case "m2":
-				setFieldValue("df_ligne_devis_masse_unitaire_ss_joint", mvp*(ep / 100));
+				setFieldValue("defiLigneDevisMasseUnitaireSsJoints", mvp*(ep / 100));
 				break;
 		}
         
         // calcul nombre d'éléments par unité avec joint
         if ((lng + dim_joint == 0) || (lrg + dim_joint == 0)){
-        	setFieldValue("df_ligne_devis_nb_elt_ac_joint", 0);
+        	setFieldValue("defiLigneDevisNombreElementsAcJoints", 0);
         }
         else {
         	switch(unite){
 				case "m2":
-					setFieldValue("df_ligne_devis_nb_elt_ac_joint", 1/((lng + dim_joint)*(lrg + dim_joint)/10000));
+					setFieldValue("defiLigneDevisNombreElementsAcJoints", 1/((lng + dim_joint)*(lrg + dim_joint)/10000));
 					break;
 				case "ml":
-					setFieldValue("df_ligne_devis_nb_elt_ac_joint", 0);
+					setFieldValue("defiLigneDevisNombreElementsAcJoints", 0);
 					break;
 				case "u":
-					setFieldValue("df_ligne_devis_nb_elt_ac_joint", 0);
+					setFieldValue("defiLigneDevisNombreElementsAcJoints", 0);
 					break;
 			}
         }
         
         // calcul masse unitaire avec joint
         if (dim_joint == 0){
-        	setFieldValue("df_ligne_devis_masse_unitaire_ac_joint", ((ep * lng * lrg) / 10000));
+        	setFieldValue("defiLigneDevisMasseUnitaireAcJoints", ((ep * lng * lrg) / 10000));
         }
         else{
-        	double n = getField("df_ligne_devis_nb_elt_ac_joint").getDouble(0);
-        	setFieldValue("df_ligne_devis_masse_unitaire_ac_joint", (n *(ep * lng * lrg * mvp / 1000000)));
+        	double n = getField("defiLigneDevisNombreElementsAcJoints").getDouble(0);
+        	setFieldValue("defiLigneDevisMasseUnitaireAcJoints", (n *(ep * lng * lrg * mvp / 1000000)));
         }
         
         // calcul poids total
-        double muaj = getField("df_ligne_devis_masse_unitaire_ac_joint").getDouble(0);
-        double musj = getField("df_ligne_devis_masse_unitaire_ss_joint").getDouble(0);
+        double muaj = getField("defiLigneDevisMasseUnitaireAcJoints").getDouble(0);
+        double musj = getField("defiLigneDevisMasseUnitaireSsJoints").getDouble(0);
 		if (muaj == 0){
-        	setFieldValue("df_ligne_devis_poids_total", ((musj * qte) / 1000));
+        	setFieldValue("defiLigneDevisPoidsTotal", ((musj * qte) / 1000));
         }
         else{
-        	setFieldValue("df_ligne_devis_poids_total", ((muaj * qte) / 1000));
+        	setFieldValue("defiLigneDevisPoidsTotal", ((muaj * qte) / 1000));
         }
         
         // calcul prix transport / unité
-        setFieldValue("df_ligne_devis_prix_transport_unitaire", muaj / 24000 * ptr);
+        setFieldValue("defiLigneDevisPrixTransportUnitaire", muaj / 24000 * ptr);
         
         // calcul nombre camions 
-        double pt = getField("df_ligne_devis_poids_total").getDouble(0);
-        setFieldValue("df_ligne_devis_nombre_camions", pt / 24 );
+        double pt = getField("defiLigneDevisPoidsTotal").getDouble(0);
+        setFieldValue("defiLigneDevisNombreCamions", pt / 24 );
         
         // calcul prix EXW / unité
         if (pexwu == 0){
-        	setFieldValue("df_ligne_devis_prix_exw_unite", pexwt * muaj / 1000);
+        	setFieldValue("defiLigneDevisPrixExwUnite", pexwt * muaj / 1000);
         }
         else {
-        	setFieldValue("df_ligne_devis_prix_exw_unite", pexwu);
+        	setFieldValue("defiLigneDevisPrixExwUnite", pexwu);
         }
         
         // calcul prix déboursé sec
-        double pexwut = getField("df_ligne_devis_prix_exw_unite").getDouble(0);
-        double ptu = getField("df_ligne_devis_prix_transport_unitaire").getDouble(0);
-        setFieldValue("df_ligne_devis_prix_debourse_sec",pexwut + ptu);
+        double pexwut = getField("defiLigneDevisPrixExwUnite").getDouble(0);
+        double ptu = getField("defiLigneDevisPrixTransportUnitaire").getDouble(0);
+        setFieldValue("defiLigneDevisPrixUnitaireSec",pexwut + ptu);
         
         // calcul total achat reference ht
-        setFieldValue("df_ligne_devis_total_achat_reference_ht", pexwut * qte);
+        setFieldValue("defiLigneDevisTotalEXWHT", pexwut * qte);
         
         // calcul total achat ht
-        double pds = getField("df_ligne_devis_prix_debourse_sec").getDouble(0);
-        setFieldValue("df_ligne_devis_total_achat_ht", pds * qte);
+        double pds = getField("defiLigneDevisPrixUnitaireSec").getDouble(0);
+        setFieldValue("defiLigneDevisTotalAchatHT", pds * qte);
         
         // calcul prix x coef
-        double coef = getField("df_ligne_devis_coef").getDouble(0);
-        setFieldValue("df_ligne_devis_prix_vente_calcule", pds * coef);
+        double coef = getField("defiLigneDevisCoef").getDouble(0);
+        setFieldValue("defiLigneDevisPrixVenteCalcule", pds * coef);
         
         // calcul prix vente imposé
-       Double pvi = getField("df_ligne_devis_prix_vente_impose").getDouble();
-       double pvc = getField("df_ligne_devis_prix_vente_calcule").getDouble(0);
+       Double pvi = getField("defiLigneDevisPrixVenteImpose").getDouble();
+       double pvc = getField("defiLigneDevisPrixVenteCalcule").getDouble(0);
        if (pvi == 0){
-       	setFieldValue("df_ligne_devis_prix_vente_impose", pvc);
+       	setFieldValue("defiLigneDevisPrixVenteImpose", pvc);
        }
        else{
-       	setFieldValue("df_ligne_devis_prix_vente_impose", pvi);
+       	setFieldValue("defiLigneDevisPrixVenteImpose", pvi);
        }
        
        
        // calcul total
 		double total = pvi * qte;
-    	setFieldValue("df_ligne_devis_prix_total_ht", total);
+    	setFieldValue("defiLigneDevisPrixTotalHT", total);
 	}
 	
 
