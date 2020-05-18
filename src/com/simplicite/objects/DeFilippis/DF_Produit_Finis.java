74d8ef955f80d027111c1398b05861d59e0bec0e
designer (Hamza SENOUSSI) <no-reply@simplicite.fr>
2020-05-18 02:34:42 +0000

    [9.9] Code Cleaning

--- src/com/simplicite/objects/DeFilippis/DF_Produit_Finis.java
+++ src/com/simplicite/objects/DeFilippis/DF_Produit_Finis.java
@@ -9,12 +9,5 @@ import com.simplicite.util.tools.*;
 */
 public class DF_Produit_Finis extends ObjectDB {
 	private static final long serialVersionUID = 1L;
-	/*
-	@Override
-	public void initUpdate() {
-		Integer id = Integer.valueOf(this.getRowId());
-		String id_pf = String.format("%04d",id);
-		
-		setFieldValue("defiPrdId",id_pf);
-	}*/
+
 }
