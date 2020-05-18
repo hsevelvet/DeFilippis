e50cc9602140699b91c3e25907de9d7a91208ebd
designer (Hamza SENOUSSI) <no-reply@simplicite.fr>
2020-05-18 02:26:39 +0000

    [9.9] Code Cleaning

--- src/com/simplicite/objects/DeFilippis/DF_Contact.java
+++ src/com/simplicite/objects/DeFilippis/DF_Contact.java
@@ -9,25 +9,16 @@ import com.simplicite.util.tools.*;
  */
 public class DF_Contact extends ObjectDB {
 	private static final long serialVersionUID = 1L;
-	/*
-	
-	@Override
-	public void initUpdate() {
-		Integer pid = Integer.valueOf(getRowId());
-		String contact_id = String.format("%04d",pid);
-		
-		setFieldValue("defiContactId",contact_id);
-	}
-	*/
+
 	@Override
 	public void initRefSelect(ObjectDB parent) {
 			
-		
+		// Sur devis : ne récupérer que les contacts correspondants au client séléctionné 
 		if(parent!=null && parent.getName().equals("DF_Devis")){
 			AppLog.info(getClass(), "initRefSelect", "------"+parent.getRowId(), getGrant());
 			setFieldFilter("DF_Contact_DF_Client_id", parent.getFieldValue("DF_Devis_DF_Client_id"));
 		}
-		
+		// Sur Commande : ne récupérer que les contact correspondants au client séléctionné 
 		if(parent!=null && parent.getName().equals("DF_Commande")){
 			AppLog.info(getClass(), "initRefSelect", "------"+parent.getRowId(), getGrant());
 			setFieldFilter("DF_Contact_DF_Fournisseurs_id", parent.getFieldValue("DF_Commande_DF_Fournisseurs_id"));
