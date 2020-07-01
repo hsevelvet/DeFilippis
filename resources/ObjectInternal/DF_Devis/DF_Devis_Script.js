1d89a1b2fdfa4841578975be7de635929d45f053
designer (Hamza SENOUSSI) <no-reply@simplicite.fr>
2020-07-01 09:32:14 +0000

    [15.2] Status Livraison Config

--- resources/ObjectInternal/DF_Devis/DF_Devis_Script.js
+++ resources/ObjectInternal/DF_Devis/DF_Devis_Script.js
@@ -5,6 +5,11 @@ var DF_Devis = typeof DF_Devis !== "undefined" ? DF_Devis : (function($) {
 			// Helper to dynamically change unit price when selecting product (also done on server side)
 			o.locals.ui.form.onload = function(ctn, obj) {
 			var email = $ui.getUIField(ctn, obj, "defiDevisContactMail");
+			var v_affaire = $ui.getUIField(ctn, obj, "DF_Devis_DF_Chantier_id__defiAfrLieuAffaire");
+			
+			v_affaire.on("change", function(){
+				$ui.getUIField(ctn, obj, "defiDevisLieuProjet").ui.val(v_affaire.ui.val());
+			});
 			
 			email.ui.on("change", function() {
 					$ui.getUIField(ctn, obj, "defiDevisCommentaire").ui.val(email.ui.val());
